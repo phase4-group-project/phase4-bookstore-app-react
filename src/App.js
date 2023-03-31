dfe3329b5dd5576daabe723026de9c6b5855b145
kevinkkimutai <kiruikevinkim@gmail.com>
2023-03-31 14:26:11 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,50 +1,38 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./pages/Login";
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import {  Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import Home from "./pages/Home";
 import NewBook from "./Components/NewBook";
+import Register from "./pages/Register";
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [books, setBooks] = useState([])
-  
+  const [token, setToken] = useState(localStorage.getItem('token') || null);
 
-  useEffect(() => {
-    fetch("http://localhost:3000/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
-
-  if (!user) return <Login onLogin={setUser} />;
-
-  // Add a book item in state
-    function handleAddBook(newBook) {
-        setBooks([...books, newBook]);
-      }
-
-  
-
+  const logout = () => {
+    localStorage.removeItem('token');
+    setToken(null);
+  };
 
   return (
-    <>
-      <BrowserRouter>
-        <header>
-          <NavBar user={user} setUser={setUser} />
-        </header>
-
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path = "newbook" element = {<NewBook onAddBook = {handleAddBook}/>}/>
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </>
+   <>
+       <Router>
+      <NavBar logout={logout} token={token} />
+      <main>
+  
+      <Routes>
+        <Route path="/login" element={!token ? <Login setToken={setToken} /> : <Home token={token} />} />
+        <Route path="/register" element={!token ? <Register setToken={setToken} /> : <Home token={token} />} />
+        {/* <Route path="/" element={token ? <Home token={token} /> : <Login setToken={setToken} />} />
+        <Route path="/books" element={token ? <NewBook token={token} /> : <Login setToken={setToken} />} /> */}
+      </Routes>
+     
+      </main>
+      </Router>
+   
+    
+   </>
   );
 }
 
